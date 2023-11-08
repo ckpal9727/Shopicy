da341fd10c08fe6baf2aa8e0323a199529c268d7
Chandrakumar Pal <cpal@altumlabs.com>
2023-11-08 12:04:28 +0530

    Commit with design 2

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import path from 'path';
 const port =8000;
 const app=express();
-app.get('/',(req,res)=>{
-    res.send(`Server is running..`)
-})
+// app.get('/',(req,res)=>{
+//     res.send(`Server is running..`)
+// })
 app.use(express.static(path.join(__dirname,'/frontend/build')))
 app.listen(port,() => console.log(`Running ${port}`))
